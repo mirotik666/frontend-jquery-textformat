b56d2c4a24f88a261d14184e96c29d07ca489fef
Galina <tris151316@gmail.com>
2017-07-13 19:42:21 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 'use strict';
+var inputArea = $('.message-form > .message-field');
+var ouputArea = $('main > .message-formatted');
+var textModifyingForm = $('main > .message-form');
 
+function handleModifyText() {
+    ouputArea.text(
+        inputArea.val().trim()
+        .replace(/[\s{2,}]+/g, ' ')
+        .toLowerCase()
+    );
+}
+
+function resetOutput() {
+    ouputArea.text('');
+}
+
+inputArea.on('keyup', handleModifyText);
+textModifyingForm.on('reset', resetOutput);
 console.log($)
